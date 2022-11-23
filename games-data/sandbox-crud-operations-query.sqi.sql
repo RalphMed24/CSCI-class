1d291699a4a730cbb98e90efa48e480fa5eb3f01
RalphMed24 <116305061+RalphMed24@users.noreply.github.com>
2022-11-22 23:38:07 -0600

    ADD games data project

---
+++ games-data/sandbox-crud-operations-query.sqi.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (7049108, 1, 'Im back');
+
+--UPDATE sandbox SET double_value = -2.242424 WHERE id = 125;
+
+--DELETE FROM sandbox WHERE id = 100;
+
+SELECT * FROM sandbox;
